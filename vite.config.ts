110236c47238cb694ee2494a5bd1e670d4ac3257
ChristianScarpati <christian-mallmann@hotmail.com>
2024-02-20 10:21:48 -0300

    fix: lint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     preprocessorOptions: {
       sass: {
         includePaths: [path.resolve(__dirname, "styles")],
-        prependData: `@import "_variables.scss";`
+        prependData: "@import '_variables.scss';"
       }
     }
   }
